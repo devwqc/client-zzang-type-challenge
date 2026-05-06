37ac8c81551dd7ad03e0a73450efbb67c0bb40f6
devwqc <devwqc@gmail.com>
2026-05-06 18:27:44 +0900

    bong(260506): [Week3] 00008-medium-readonly-2-re

---
+++ bongchan/00008-medium-readonly-2-re.ts
@@ -0,0 +1,96 @@
+/*
+  8 - Readonly 2
+  -------
+  by Anthony Fu (@antfu) #보통 #readonly #object-keys
+
+  ### 질문
+
+  `T`에서 `K` 프로퍼티만 읽기 전용으로 설정해 새로운 오브젝트 타입을 만드는 제네릭 `MyReadonly2<T, K>`를 구현하세요. `K`가 주어지지 않으면 단순히 `Readonly<T>`처럼 모든 프로퍼티를 읽기 전용으로 설정해야 합니다.
+
+  예시:
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  const todo: MyReadonly2<Todo, 'title' | 'description'> = {
+    title: "Hey",
+    description: "foobar",
+    completed: false,
+  }
+
+  todo.title = "Hello" // Error: cannot reassign a readonly property
+  todo.description = "barFoo" // Error: cannot reassign a readonly property
+  todo.completed = true // OK
+  ```
+
+  > GitHub에서 보기: https://tsch.js.org/8/ko
+*/
+
+// 🚀 시작: 2026-05-06 18:20
+// ✅ 종료: 2026-05-06 18:22
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. 제네릭 K 를 keyof T 로 타입 제한
+      - error 케이스 대응
+
+    2. 제네릭 K 에 기본값 keyof T 를 넣어 옵셔널 처리
+      - 테스트 케이스 1번 대응
+      - K 가 주어지지 않으면 T 의 모든 키는 readonly 처리 필요
+        - K 를 Mapped Types 로 readonly 처리하고, Intersection Type 으로 K 에 포함되지 않는 프로퍼티를 Mapped Types 로 처리
+
+  😆 배움
+    -
+
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Alike, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'description'>, Expected>>,
+];
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, 'title' | 'invalid'>;
+
+interface Todo1 {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
+}
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/8/answer/ko
+  > 정답 보기: https://tsch.js.org/8/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
