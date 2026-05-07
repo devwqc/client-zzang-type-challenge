03b1ec4a20caddc13ac8ce689b34542d4d4be418
kyukyu <142164931+kyukyu-dev@users.noreply.github.com>
2026-05-07 13:13:51 +0900

    kyu: [Week2] 03057-easy-push

---
+++ kyuyoung/03057-easy-push.ts
@@ -0,0 +1,22 @@
+type Push<T extends any[], U> = [...T, U]
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Expect<Equal<Push<number[], string>, string[]>>,
+  // @ts-expect-error
+  Expect<Equal<Push<string[], number>, [string, number]>>,
+]
+
+/*
+  배운점
+  -
+*/
