fb40f5c8832059019079f6b6d218752bef78f202
kyukyu <142164931+kyukyu-dev@users.noreply.github.com>
2026-05-07 13:07:03 +0900

    kyu: [Week2] 00189-easy-awaited

---
+++ kyuyoung/00189-easy-awaited.ts
@@ -0,0 +1,24 @@
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+type Z1 = Promise<Promise<Promise<string | boolean>>>
+type T = { then: (onfulfilled: (arg: number) => any) => any }
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>,
+]
+
+/*
+  배운점
+  - 타입 선언도 재귀적으로 가능
+  - then 메서드만 가지는 PromiseLike 타입
+*/
