b36c10ebe64d26cccf095bf56a7270c389b8b2c0
정봉찬 <devwqc@gmail.com>
2026-05-09 13:17:08 +0900

    bong(260509): [Week4] 00016-medium-pop

---
+++ bongchan/00016-medium-pop.ts
@@ -0,0 +1,61 @@
+/*
+  16 - Pop
+  -------
+  by Anthony Fu (@antfu) #보통 #array
+
+  ### 질문
+
+  > 이 챌린지에는 TypeScript 4.0 사용이 권장됩니다.
+
+  배열 `T`를 사용해 마지막 요소를 제외한 배열을 반환하는 제네릭 `Pop<T>`를 구현합니다.
+
+  예시
+
+  ```ts
+  type arr1 = ['a', 'b', 'c', 'd']
+  type arr2 = [3, 2, 1]
+
+  type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+  type re2 = Pop<arr2> // expected to be [3, 2]
+  ```
+
+  **더보기**: 비슷하게 `Shift`, `Push` 그리고 `Unshift`도 구현할 수 있을까요?
+
+  > GitHub에서 보기: https://tsch.js.org/16/ko
+*/
+
+// 🚀 시작: 2026-05-09 13:10
+// ✅ 종료: 2026-05-09 13:14
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. infer 사용
+      - type Pop<T extends any[]> = T extends [...infer Rest, any] ? [...Rest] : never;
+        - Expect<Equal<Pop<[]>, []>> 케이스에서 에러 발생
+          - [...infer Rest, any] 를 만족하지 않을 때 never 가 아닌 [] 를 반환해보자
+
+  😆 배움
+  -
+  
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type Pop<T extends any[]> = T extends [...infer Rest, any] ? [...Rest] : [];
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
+];
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/16/answer/ko
+  > 정답 보기: https://tsch.js.org/16/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
