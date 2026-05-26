668981fe723090526058b9a6672f636611606a00
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2026-05-26 16:52:00 +0900

    minji: 00002-medium-return-type-re

---
+++ minji/00002-medium-return-type-re.ts
@@ -0,0 +1,69 @@
+/*
+  2 - Get Return Type
+  -------
+  by Anthony Fu (@antfu) #medium #infer #built-in
+
+  ### Question
+
+  Implement the built-in `ReturnType<T>` generic without using it.
+
+  For example
+
+  ```ts
+  const fn = (v: boolean) => {
+    if (v)
+      return 1
+    else
+      return 2
+  }
+
+  type a = MyReturnType<typeof fn> // should be "1 | 2"
+  ```
+
+  > View on GitHub: https://tsch.js.org/2
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+]
+
+type ComplexObject = {
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
+
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, w: any) => v ? 1 : 2
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2/answer
+  > View solutions: https://tsch.js.org/2/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+<T extends (...args: any[]) => any>
+: 이 타입은 함수 타입만 받는다는 뜻
+- ...args: any[] : 아무 인자 타입 가능
+- () => any : 아무 반환 타입 가능
+
+T extends (...args: any[]) => infer R ? R : never
+- T가 (...args: any[]) => infer R에 할당 가능하면 R, 아니면 never
+  - (...args: any[]) : 인자는 임의로 가능
+  - infer R : 반환되는 값은 R이라고 칭한다 => 변수처럼 R을 사용
+*/
