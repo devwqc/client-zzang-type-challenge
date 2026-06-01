9d2f9e8adb5d2e46a545abc25fa835b3c4a442d4
정봉찬 <devwqc@gmail.com>
2026-06-02 00:13:07 +0900

    bong(260602): [Week8] 01042-medium-isnever

---
+++ bongchan/01042-medium-isnever.ts
@@ -0,0 +1,63 @@
+/*
+  1042 - IsNever
+  -------
+  by hiroya iizuka (@hiroyaiizuka) #보통 #union #utils
+
+  ### 질문
+
+  input type으로 `T`를 받는 IsNever type을 구현하세요. 만약 `T`의 유형이 `never`으로 확인되면 `true`를 반환하고 아니면 `false`를 반환합니다
+
+  예시:
+
+  ```ts
+  type A = IsNever<never> // expected to be true
+  type B = IsNever<undefined> // expected to be false
+  type C = IsNever<null> // expected to be false
+  type D = IsNever<[]> // expected to be false
+  type E = IsNever<number> // expected to be false
+  ```
+
+  > GitHub에서 보기: https://tsch.js.org/1042/ko
+*/
+
+// 🚀 시작: 2026-06-02 00:05
+// ✅ 종료: 2026-06-02 00:06
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. 제네릭 T를 튜플로 감싸서 [never]로 타입 좁히기
+
+      type IsNever<T> = [T] extends [never] ? true : false;
+
+  😆 배움
+    1. never는 공집합 유니온으로 취급
+      - never를 extends 절의 왼쪽에서 naked type parameter로 사용되면 공집합 유니온으로 조건부 타입 전체가 never로 평가
+      - type Bad<T> = T extends never ? true : false;
+      - type R = Bad<never>; // never
+  
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type IsNever<T> = [T] extends [never] ? true : false;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>,
+];
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/1042/answer/ko
+  > 정답 보기: https://tsch.js.org/1042/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
