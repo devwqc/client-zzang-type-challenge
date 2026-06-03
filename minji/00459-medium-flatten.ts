759a0f871d106221fa0c3a12ff2fa601c572d4d9
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2026-06-03 18:31:27 +0900

    minji: [Week6] 00459-medium-flatten

---
+++ minji/00459-medium-flatten.ts
@@ -0,0 +1,52 @@
+/*
+  459 - Flatten
+  -------
+  by zhouyiming (@chbro) #medium #array
+
+  ### Question
+
+  In this challenge, you would need to write a type that takes an array and emitted the flatten array type.
+
+  For example:
+
+  ```ts
+  type flatten = Flatten<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, 5]
+  ```
+
+  > View on GitHub: https://tsch.js.org/459
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Flatten<T extends any[]> = T extends [infer First, ...infer Rest] ? First extends any[] ? Flatten<[...First, ...Rest]> : [First, ...Flatten<Rest>] : []
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<Flatten<[{ foo: 'bar', 2: 10 }, 'foobar']>, [{ foo: 'bar', 2: 10 }, 'foobar']>>,
+]
+
+// @ts-expect-error
+type error = Flatten<'1'>
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/459/answer
+  > View solutions: https://tsch.js.org/459/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+접근
+
+배열을 모두 flat하게 풀어야 함
+
+1. type Flatten<T extends any[]> = T extends [infer First, ...infer Rest] ? First extends any[] ? Flatten<[...First, Rest]> : [First, ...Flatten<Rest>] : []
+=> Flatten<[1, [2]]> - 무한 루프 발생
+=> 해결: Rest를 spread해서 넘겨주기
+ */
